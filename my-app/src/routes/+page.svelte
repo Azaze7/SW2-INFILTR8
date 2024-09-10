53ddd26217a2b5b90298dab7c7354f166c81bc39
Brian Mata <37246371+donburgers@users.noreply.github.com>
2024-09-10 09:42:23 -0600

    Login Button with animations with prompts

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,15 @@
 <script>
 	import Sparkles from '$lib/components/ui/Sparkles/Sparkles.svelte';
+	import { IconSquareRoundedX } from '@tabler/icons-svelte';
+	import MultiStepLoader from '$lib/components/ui/MultiStepLoader/MultiStepLoader.svelte';
+	let loading = false;
+
+const loadingStates = [
+	{
+		text: 'POGGERS'
+	}
+];
+
 </script>
 
 <div class="relative flex items-center justify-center h-screen w-screen overflow-hidden">
@@ -16,6 +26,26 @@
 	<div class="relative z-10 flex flex-col items-center justify-center text-center text-black">
 		<h2 class="text-3xl font-bold md:text-7xl lg:text-9xl">
 			INFILTR8
+			<div class="flex h-[60vh] w-full items-center justify-center">
+				<!-- Core Loader Modal  -->
+				<MultiStepLoader {loadingStates} {loading} duration={2000} />
+				<button
+					on:click={() => (loading = true)}
+					class="mx-auto flex h-10 items-center justify-center rounded-lg bg-[#39C3EF] px-8 text-sm font-medium text-black transition duration-200 hover:bg-[#39C3EF]/90 md:text-base"
+					style="box-shadow: 0px -1px 0px 0px #ffffff40 inset, 0px 1px 0px 0px #ffffff40 inset;"
+				>
+					Login
+				</button>
+			
+				{#if loading}
+					<button
+						class="fixed right-4 top-4 z-[120] text-black dark:text-white"
+						on:click={() => (loading = false)}
+					>
+						<IconSquareRoundedX class="h-10 w-10" />
+					</button>
+				{/if}
+			</div>
 		</h2>
 		
 		<div class="relative mt-8 w-[40rem]">
