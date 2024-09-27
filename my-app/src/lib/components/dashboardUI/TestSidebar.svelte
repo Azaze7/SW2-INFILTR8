c22acf50d6a549bce88c5b5dcac484bbbe74776f
carevilla <carevilla@miners.utep.edu>
2024-09-27 11:44:36 -0600

    layout done time to style

--- my-app/src/lib/components/dashboardUI/TestSidebar.svelte
+++ my-app/src/lib/components/dashboardUI/TestSidebar.svelte
@@ -20,7 +20,10 @@
                 <div class="trigger">
                     <!-- svelte-ignore a11y-invalid-attribute -->
                     <!-- svelte-ignore a11y-missing-content -->
-                    <a href="#"></a>
+                    <a href="#">
+                        <ion-icon name="menu-outline"></ion-icon>
+                        <ion-icon name="chevron-forward-outline"></ion-icon>
+                    </a>
                 </div>
             </div>
             <div class="brand">
@@ -29,47 +32,129 @@
                         <li>
                             <!-- svelte-ignore a11y-invalid-attribute -->
                             <a href="#">
+                                <ion-icon name="grid-outline"></ion-icon>
                                 <span>Dashboard</span>
                             </a>
                         </li>
-                        <li>
+                        <li class="has-child">
                             <!-- svelte-ignore a11y-invalid-attribute -->
                             <a href="#">
+                                <ion-icon name="prism-outline"></ion-icon>
                                 <span>Projects</span>
+                                <ion-icon name="chevron-forward-outline"></ion-icon>
                             </a>
                             <ul>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">My Projects</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Goals</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Budgets</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Files</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Settings</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Funds</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Security</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
                                 <li><a href="#">Referals</a></li>
                             </ul>
                         </li>
                         <li>
                             <!-- svelte-ignore a11y-invalid-attribute -->
                             <a href="#">
-                                <span></span>
+                                <ion-icon name="albums-outline"></ion-icon>
+                                <span>Campaign</span>
+                            </a>
+                        </li>
+                        <li class="small">Performance</li>
+                        <li class="has-child">
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <ion-icon name="pulse-outline"></ion-icon>
+                                <span>Analytics</span>
+                                <ion-icon name="chevron-forward-outline"></ion-icon>
+                            </a>
+                            <ul>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">My Projects</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Goals</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Budgets</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Files</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Settings</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Funds</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Security</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Referals</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Referals</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Referals</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-child">
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <ion-icon name="mail-unread-outline"></ion-icon>
+                                <span>Message</span>
+                                <ion-icon name="chevron-forward-outline"></ion-icon>
+                            </a>
+                            <ul>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Inobx</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Sent</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Starred</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Budgets</a></li>
+                            </ul>
+                        </li>
+                        <li class="has-child">
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <ion-icon name="person-circle-outline"></ion-icon>
+                                <span>Account</span>
+                                <ion-icon name="chevron-forward-outline"></ion-icon>
+                            </a>
+                            <ul>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Blog</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Settings</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Security</a></li>
+                                <!-- svelte-ignore a11y-invalid-attribute -->
+                                <li><a href="#">Referals</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <ion-icon name="wallet-outline"></ion-icon>
+                                <span>Subscriptions</span>
                             </a>
                         </li>
                         <li>
                             <!-- svelte-ignore a11y-invalid-attribute -->
                             <a href="#">
-                                <span></span>
+                                <ion-icon name="settings-outline"></ion-icon>
+                                <span>Configurations</span>
                             </a>
                         </li>
                         <li>
                             <!-- svelte-ignore a11y-invalid-attribute -->
                             <a href="#">
-                                <span></span>
-                            </a>
-                        </li>
-                        <li>
-                            <!-- svelte-ignore a11y-invalid-attribute -->
-                            <a href="#">
-                                <span></span>
+                                <ion-icon name="information-circle-outline"></ion-icon>
+                                <span>Help & Settings</span>
                             </a>
                         </li>
                     </ul>
@@ -78,7 +163,7 @@
             <div class="sign-out">
                 <!-- svelte-ignore a11y-invalid-attribute -->
                 <a href="#">
-                    <span></span>
+                    <span>Sign Out</span>
                 </a>
             </div>
         </div>
@@ -86,6 +171,8 @@
             <h1>Content</h1>
         </div>
     </div>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
 </body>
 
 
