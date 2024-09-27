13e6ef9c91068532e1caf27f016d964721a64d38
carevilla <carevilla@miners.utep.edu>
2024-09-27 10:52:39 -0600

    commented out style code and redesigning dashboard

---
+++ my-app/src/lib/components/dashboardUI/TestSidebar.svelte
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive TestSidebar</title>
+</head>
+
+<body>
+    <div id="page" class="site">
+        <div class="sidebar">
+            <div class="brand">
+                <div class="logo">
+                    <!-- svelte-ignore a11y-invalid-attribute -->
+                    <a href="#">
+                        <span>.web</span>design
+                    </a>
+                </div>
+                <div class="trigger">
+                    <!-- svelte-ignore a11y-invalid-attribute -->
+                    <!-- svelte-ignore a11y-missing-content -->
+                    <a href="#"></a>
+                </div>
+            </div>
+            <div class="brand">
+                <nav>
+                    <ul>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span>Dashboard</span>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span>Projects</span>
+                            </a>
+                            <ul>
+                                <li><a href="#">My Projects</a></li>
+                                <li><a href="#">Goals</a></li>
+                                <li><a href="#">Budgets</a></li>
+                                <li><a href="#">Files</a></li>
+                                <li><a href="#">Settings</a></li>
+                                <li><a href="#">Funds</a></li>
+                                <li><a href="#">Security</a></li>
+                                <li><a href="#">Referals</a></li>
+                            </ul>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span></span>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span></span>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span></span>
+                            </a>
+                        </li>
+                        <li>
+                            <!-- svelte-ignore a11y-invalid-attribute -->
+                            <a href="#">
+                                <span></span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="sign-out">
+                <!-- svelte-ignore a11y-invalid-attribute -->
+                <a href="#">
+                    <span></span>
+                </a>
+            </div>
+        </div>
+        <div class="main">
+            <h1>Content</h1>
+        </div>
+    </div>
+</body>
+
+
+</html>
