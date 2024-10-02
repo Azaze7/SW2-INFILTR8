5da2911f083aa2af9158512ca02e1b8b4c787935
Luis D. Sanchez <123500228+ldsanchez3@users.noreply.github.com>
2024-10-01 19:06:09 -0600

    Update Settings.svelte

--- my-app/src/lib/components/dashboardUI/Settings.svelte
+++ my-app/src/lib/components/dashboardUI/Settings.svelte
@@ -10,7 +10,9 @@
     //Import the sidebar and top navbar made by other teammates.
         //Kept to ensure consistency accross the the site.
     import Navbar from "$lib/components/dashboardUI/Navbar.svelte";
-    import Sidebar from "$lib/components/dashboardUI/Sidebar.svelte";
+    //***DISABLE OLD SIDEBAR AND LOAD NEW ONE***/
+    //import Sidebar from "$lib/components/dashboardUI/Sidebar.svelte";
+    import TestSidebar from "$lib/components/dashboardUI/TestSidebar.svelte";
     //Import onMount, which allows for functions to be run when the user clicks buttons on the settings menu.
         //This includes enabling darkmode, changing font size, etc.
     import { onMount } from 'svelte';
