020f035313698d0a374af30407736ec24fd42792
carevilla <carevilla@miners.utep.edu>
2024-10-08 15:15:35 -0600

    pulled updated db code

--- INFILTR8/src/routes/dashboard/+layout.svelte
+++ INFILTR8/src/routes/dashboard/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-	import '/src/app.postcss';
-	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import '../../app.postcss';
+	import { AppShell, AppBar, LightSwitch } from '@skeletonlabs/skeleton';
 	import { AppRail, AppRailTile, AppRailAnchor } from '@skeletonlabs/skeleton';
 	let currentTile: number = 0;
+	import { page } from '$app/stores';
+	import { FileDropzone } from '@skeletonlabs/skeleton';
 	// Highlight JS
 	import hljs from 'highlight.js/lib/core';
 	import 'highlight.js/styles/github-dark.css';
@@ -11,6 +13,11 @@
 	import css from 'highlight.js/lib/languages/css';
 	import javascript from 'highlight.js/lib/languages/javascript';
 	import typescript from 'highlight.js/lib/languages/typescript';
+
+	// Christian R Imports
+	import { TabGroup, Tab, TabAnchor } from '@skeletonlabs/skeleton';
+	import { popup } from '@skeletonlabs/skeleton';
+	import type { PopupSettings } from '@skeletonlabs/skeleton';
 
 	hljs.registerLanguage('xml', xml); // for HTML
 	hljs.registerLanguage('css', css);
@@ -36,6 +43,29 @@
     } else {
         greeting = "Good evening!";
     }
+
+
+
+
+	const AccountPopUp: PopupSettings = {
+		// Represents the type of event that opens/closed the popup
+		event: 'click',
+		// Matches the data-popup value on your popup element
+		target: 'AccountPopUp',
+		// Defines which side of your trigger the popup will appear
+		placement: 'bottom',
+	};
+
+	const NotificationPopUp: PopupSettings = {
+		// Represents the type of event that opens/closed the popup
+		event: 'click',
+		// Matches the data-popup value on your popup element
+		target: 'NotificationPopUp',
+		// Defines which side of your trigger the popup will appear
+		placement: 'bottom',
+	};
+					
+
 </script>
 
 <!-- App Shell -->
@@ -48,29 +78,29 @@
 				<strong class="text-xl uppercase"><p>{greeting}</p></strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
+				<section class="p-4">
+					<LightSwitch />
+				</section>
+				<button class="btn btn-sm variant-ghost-surface" use:popup={NotificationPopUp}>Notifications</button>
+				<div class="card p-4 w-72 shadow-xl" data-popup="NotificationPopUp">
+					<div><p>Notification Details Here</p></div>
+					<div class="arrow bg-surface-100-800-token" />
+				</div>
+
+				<button class="btn btn-sm variant-ghost-surface" use:popup={AccountPopUp}>Account</button>
+				<div class="card p-4 w-72 shadow-xl" data-popup="AccountPopUp">
+					<div><p>Account Details Here</p></div>
+					<div class="arrow bg-surface-100-800-token" />
+				</div>
+					
+		
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					Sign Out
 				</a>
 			</svelte:fragment>
 		</AppBar>
@@ -80,20 +110,23 @@
 			<svelte:fragment slot="sidebarLeft">
 				<AppRail>
 					<svelte:fragment slot="lead">
-						<AppRailAnchor href="/" >(icon)</AppRailAnchor>
+						<AppRailAnchor href="../analysis" >(icon)</AppRailAnchor>
+						<span>Analyze</span>
 					</svelte:fragment>
 					<!-- --- -->
 					<AppRailTile bind:group={currentTile} name="tile-1" value={0} title="tile-1">
 						<svelte:fragment slot="lead">(icon)</svelte:fragment>
-						<span>Tile 1</span>
+						<AppRailAnchor href="/dashboard" ></AppRailAnchor>
+						<span>Dashboard</span>
 					</AppRailTile>
 					<AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
 						<svelte:fragment slot="lead">(icon)</svelte:fragment>
-						<span>Tile 2</span>
+						<AppRailAnchor href="../projects" ></AppRailAnchor>
+						<span>Projects</span>
 					</AppRailTile>
 					<AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
 						<svelte:fragment slot="lead">(icon)
-							<AppRailAnchor href="src/routes/pagesettings" ></AppRailAnchor>
+							<AppRailAnchor href="/" ></AppRailAnchor>
 						</svelte:fragment>
 						<span>Settings</span>
 					</AppRailTile>
@@ -108,4 +141,3 @@
 	<!-- Page Route Content -->
 	<slot />
 </AppShell>
-
