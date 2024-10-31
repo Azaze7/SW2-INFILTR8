32fecbec95c4fe70847daf69cab3a2b9060c1f95
jbguzman <jbguzman@miners.utep.edu>
2024-10-31 14:16:26 -0600

    Added container for buttons at the bottom of the sidebar using Tailwind

--- INFILTR8/src/routes/Logs/+layout.svelte
+++ INFILTR8/src/routes/Logs/+layout.svelte
@@ -165,7 +165,14 @@
                         {/each}
                     </div>
                 </div>
-                <div>
+                <div class="flex flex-col items-center space-y-4 p-4 bg-[#111827]">
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                        Button 1
+                    </button>
+                    <button class="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+                        Button 2
+                    </button>
+                
                     <!--<LightSwitch />-->
                     <SidebarLink
                         link={{
@@ -178,10 +185,6 @@
             </Sidebar>
         </div>
     </svelte:fragment>
-
-
-
-
 
     <!-- Page Route Content -->
     <slot />
