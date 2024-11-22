024edba03bf327c558ba9c8e88078a5626866581
kakocian <166936820+kakocian@users.noreply.github.com>
2024-11-21 20:32:39 -0700

    more comments

--- INFILTR8/src/routes/Login/+page.svelte
+++ INFILTR8/src/routes/Login/+page.svelte
@@ -1,3 +1,8 @@
+<!-- The Frontend Login Page provides a simple and focused 
+ interface for user authentication. It utilizes the `Authenticate` 
+ component to display the login UI, allowing users to log in and 
+ gain access to the application. This minimalistic design ensures a 
+ straightforward and seamless authentication process for users. -->
 <script>
     import Authenticate from "$lib/components/loginUI/Authenticate.svelte";
 </script>
